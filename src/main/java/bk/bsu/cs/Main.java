7af48b0aa103219cee37fc8e1291e12b5f6370d5
Bjorn <b.keymis@gmail.com>
2021-09-06 00:25:31 +0200

    Endringer

--- src/main/java/bk/bsu/cs/Main.java
+++ src/main/java/bk/bsu/cs/Main.java
@@ -56,6 +56,7 @@ public class Main extends Application {
         rbRdm4.setToggleGroup(tgrb);
         Label lblLevels = new Label("Levels: ");
         TextField tfLevels = new TextField();
+        tfLevels.setText("25");
         tfLevels.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue.matches("\\d*")) {
                 tfLevels.setText(newValue.replaceAll("[^\\d]", ""));
@@ -65,6 +66,7 @@ public class Main extends Application {
         ComboBox<Integer> comboBox = new ComboBox<>();
         Integer[] values = {2,4,6,8,10,12,14,16,18,20};
         comboBox.setItems(FXCollections.observableArrayList(values));
+        comboBox.setPromptText("10");
         Button btnTree = new Button("Make Tree");
         btnTree.setOnAction(e->{
             int defLevels = 25;
@@ -78,20 +80,18 @@ public class Main extends Application {
             }
             treePane.getChildren().clear();
             if (rbRdm2.isSelected()){
-                //makeBranchR(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 0);
-                makeBranchR(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels);
+                makeBranchRan(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 60);
             }else if (rbRdm3.isSelected()){
-                //makeBranchR3(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 0);
-                makeBranchR3(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels);
+                makeBranchRan(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 90);
             }else if (rbRdm4.isSelected()){
-                makeBranchR4(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels);
+                makeBranchRan(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 100);
             }else{
-                makeBranch(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 0);
+                makeBranchRan(window.getWidth() / 2, window.getHeight(), 120, 90, minLength, defLevels, 30);
             }
 
         });
 
-        menu.getChildren().addAll(lblRdm, rbRdm1, rbRdm2, rbRdm3, rbRdm4, lblLevels, tfLevels, btnTree, lblMLength, comboBox);
+        menu.getChildren().addAll(lblRdm, rbRdm1, rbRdm2, rbRdm3, rbRdm4, lblLevels, tfLevels, lblMLength, comboBox, btnTree);
         return menu;
     }
 
@@ -118,90 +118,32 @@ public class Main extends Application {
     }
 
     /**
-     * Rekursiv metode som lager en grein med ingen random verdier, metoden vil fortsette n antall nivåer eller til min length er nådd
-     * default minlength er satt till 10 pixler og kan endres ved hjelp av length variablen
-     * @param x X plaseringen av første grein
-     * @param y Y plaseringen av første grein
-     * @param length lengde av første grein
-     * @param angle vinkel av første grein
-     * @param minLength minste lengde siste grein kan være
-     * @param levels hjelpe variabel for nivåer må være 0
-     * @param n maks antall nivåer
+     * rekursiv metode for å lage grein på treet
+     * @param x double: start x plassering
+     * @param y double: start y plassering
+     * @param length double: start lengde
+     * @param angle double: start vinkel for treet
+     * @param minLength double: start lengde for treet
+     * @param levels int: max antall nivåer
+     * @param chance int: tilfeldighets sjanse
      */
-    public void makeBranch(double x, double y, double length, double angle, double minLength, int levels, int n){
-        if (minLength > length || n == levels){
-
-        }else{
-            n++;
-            makeBranch(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + 20, minLength, levels, n);
-            makeBranch(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + -20, minLength, levels, n);
-            Line line = new Line(x, y, beregnX(x, length, angle), beregnY(y,length,angle));
-            treePane.getChildren().add(line);
-        }
-    }
-    /**
-     * Rekursiv metode som lager en grein med nivå 1 random verdier (grein vinkel er tilfeldig), metoden vil fortsette n antall nivåer eller til min length er nådd
-     * default minlength er satt till 10 pixler og kan endres ved hjelp av length variablen
-     * @param x X plaseringen av første grein
-     * @param y Y plaseringen av første grein
-     * @param length lengde av første grein
-     * @param angle vinkel av første grein
-     * @param minLength minste lengde siste grein kan være
-     * @param levels hjelpe variabel for nivåer må være 0
-     */
-    public void makeBranchR(double x, double y, double length, double angle, double minLength, int levels){
-        if (minLength > length || curLevel == levels){
-
-        }else{
-            levels++;
-            makeBranchR(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + Math.random()*30, minLength, levels);
-            makeBranchR(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + Math.random()*-30, minLength, levels);
-            Line line = new Line(x, y, beregnX(x, length, angle), beregnY(y,length,angle));
-            treePane.getChildren().add(line);
-        }
-    }
-    /**
-     * Rekursiv metode som lager en grein med nivå 2 random verdier (grein vinkel, lengde på grein), metoden vil fortsette n antall nivåer eller til min length er nådd
-     * default minlength er satt till 10 pixler og kan endres ved hjelp av length variablen
-     * @param x X plaseringen av første grein
-     * @param y Y plaseringen av første grein
-     * @param length lengde av første grein
-     * @param angle vinkel av første grein
-     * @param minLength minste lengde siste grein kan være
-     * @param levels hjelpe variabel for nivåer må være 0
-     */
-    public void makeBranchR3(double x, double y, double length, double angle, double minLength, int levels){
-        if (minLength > length || curLevel == levels){
-
-        }else{
-            curLevel++;
-            makeBranchR3(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*30, minLength, levels);
-            makeBranchR3(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*-30, minLength, levels);
-            Line line = new Line(x, y, beregnX(x, length, angle), beregnY(y,length,angle));
-            treePane.getChildren().add(line);
-        }
-    }
-    /**
-     * Rekursiv metode som lager en grein med nivå 3 random verdier (grein vinkel, lengde på grein og antall grein (1 eller 2))), metoden vil fortsette n antall nivåer eller til min length er nådd
-     * default minlength er satt till 10 pixler og kan endres ved hjelp av length variablen
-     * @param x X plaseringen av første grein
-     * @param y Y plaseringen av første grein
-     * @param length lengde av første grein
-     * @param angle vinkel av første grein
-     * @param minLength minste lengde siste grein kan være
-     * @param levels hjelpe variabel for nivåer må være 0
-     */
-    public void makeBranchR4(double x, double y, double length, double angle, double minLength, int levels){
+    public void makeBranchRan(double x, double y, double length, double angle, double minLength, int levels, int chance){
         if (minLength > length || levels == curLevel){
-
         }else{
-            if (randint.nextInt(4) == 1){
-                makeBranchR4(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*30, minLength, levels);
-            }else if (randint.nextInt(4) == 2){
-                makeBranchR4(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*-30, minLength, levels);
-            }else {
-                makeBranchR4(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*30, minLength, levels);
-                makeBranchR4(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*-30, minLength, levels);
+            int rolledValue = randint.nextInt(chance);
+            if (rolledValue < 30){
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + 20, minLength, levels, chance);
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + -20, minLength, levels, chance);
+            }else if (rolledValue > 30 && rolledValue < 60){
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + Math.random()*30, minLength, levels, chance);
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * 0.8, angle + Math.random()*-30, minLength, levels, chance);
+            }else if (rolledValue > 60 && rolledValue < 90){
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*-30, minLength, levels, chance);
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*30, minLength, levels, chance);
+            }else if (rolledValue > 90 && rolledValue < 95){
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*30, minLength, levels, chance);
+            }else{
+                makeBranchRan(beregnX(x, length, angle), beregnY(y, length, angle), length * Math.random(), angle + Math.random()*-30, minLength, levels, chance);
             }
             curLevel++;
             Line line = new Line(x, y, beregnX(x, length, angle), beregnY(y,length,angle));
